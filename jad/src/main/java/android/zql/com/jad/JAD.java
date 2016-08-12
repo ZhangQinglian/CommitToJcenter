1c2b6d62d7518558ef3d7403a17bdeb6fe222a5a
Scott Zhang <qinglian.zhang@dewmobile.net>
2016-08-12 09:26:13 +0800

    jad

---
+++ jad/src/main/java/android/zql/com/jad/JAD.java
@@ -0,0 +1,14 @@
+package android.zql.com.jad;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by scott on 8/11/16.
+ */
+public final class JAD {
+
+    public static final void sayHello(Context context){
+        Toast.makeText(context,"hello man",Toast.LENGTH_LONG).show();
+    }
+}
